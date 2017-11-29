24bd756e44f5e1f92678bcf3150c8bbf6be6c0b2
guanxiaoda <guanxiaoda@meituan.com>
2017-11-29 15:59:48 +0800

    服务注册中心server

--- service-center/src/main/java/cn/guanxiaoda/servicecenter/ServiceCenterApplication.java
+++ service-center/src/main/java/cn/guanxiaoda/servicecenter/ServiceCenterApplication.java
@@ -2,11 +2,13 @@ package cn.guanxiaoda.servicecenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceCenterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceCenterApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceCenterApplication.class, args);
+  }
 }
